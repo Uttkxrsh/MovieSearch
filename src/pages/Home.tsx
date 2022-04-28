3ee9b5e580b1e03e0ae0d6f53e8188081d5cfd2b
Theo Sandell <theo01sandell@gmail.com>
2022-04-28 16:19:41 +0200

    Created styling for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
 import { RouteComponentProps } from "@gatsbyjs/reach-router";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import styles from "../style/Home.module.scss";
 
 interface Props extends RouteComponentProps {}
 
 const Home = ({}: Props) => {
-  return <h1>Home</h1>;
+  const [query, setQuery] = useState<string>("");
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Search for a movie</h1>
+        <div className={styles.formContainer}>
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+            />
+            <AiOutlineSearch size="26px" color="#ffffff" />
+          </div>
+          <div className={styles.searchResults}>{query}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
