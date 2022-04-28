875201d5bd5fc909d5a1c394151dd5a7b6ac9dd5
Theo Sandell <theo01sandell@gmail.com>
2022-04-28 21:39:42 +0200

    Add clear button, list search results

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import { RouteComponentProps } from "@gatsbyjs/reach-router";
+import { Link, navigate, RouteComponentProps } from "@gatsbyjs/reach-router";
 import { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 import styles from "../style/Home.module.scss";
 import Movie from "../types/Movie";
 import searchMovie from "../utils/api/searchMovie";
@@ -37,14 +39,43 @@ const Home = ({}: Props) => {
               onChange={(e) => setQuery(e.target.value)}
               value={query}
             />
-            <AiOutlineSearch size="26px" color="#ffffff" />
+            <div>
+              {query.length > 0 && (
+                <IoClose
+                  size="24px"
+                  color="white"
+                  onClick={() => setQuery("")}
+                />
+              )}
+              <AiOutlineSearch
+                size="26px"
+                color="#ffffff"
+                onClick={() => navigate(`/search/${query}`)}
+              />
+            </div>
           </div>
           {searchResults && (
             <div className={styles.searchResults}>
               {searchResults.map((movie: Movie) => (
-                <div className={styles.movie} key={movie.id}>
-                  {movie.title}
-                </div>
+                <Link
+                  to={`/movie/${movie.id}`}
+                  key={movie.id}
+                  className={styles.movie}
+                >
+                  <div>
+                    <div className={styles.imgWrapper}>
+                      <img
+                        src={
+                          movie.poster_path
+                            ? `https://image.tmdb.org/t/p/w92/${movie.poster_path}`
+                            : "https://via.placeholder.com/100x150"
+                        }
+                      />
+                    </div>
+                    <p>{movie.title}</p>
+                  </div>
+                  <IoIosArrowForward size="24px" color="white" />
+                </Link>
               ))}
             </div>
           )}
