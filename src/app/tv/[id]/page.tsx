95653c2d36ff6b9177631fe34eb0d45c1dfeea57
Theo Sandell <theo01sandell@gmail.com>
2023-02-08 11:27:07 +0100

    add tv page
    needs refactor

--- src/app/tv/[id]/page.tsx
+++ src/app/tv/[id]/page.tsx
@@ -1,15 +1,85 @@
-import { FC } from "react";
+import { IProps } from "./TvPage.types";
+import * as S from "./TvPage.style";
+import { NextPage } from "next";
+import urlBuilder from "@/utils/urlBuilder";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoMdStar } from "react-icons/io";
+import { paramCase } from "change-case";
+import tmdbLogo from "@/assets/tmdb_logo.svg";
+import ITv from "@/types/ITv";
 
-interface Props {
-  params: {
-    id: string;
-  };
-}
+const getTv = async (id: string): Promise<ITv> => {
+  const request = await fetch(urlBuilder(`/api/tv`, { id }));
 
-const Tv: FC<Props> = ({ params }) => {
-  const { id } = params;
+  if (request.status === 404) {
+    throw new Error("404 TV Show not found");
+  }
 
-  return <h1>TV {id}</h1>;
+  if (!request.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const result = await request.json();
+
+  return result;
 };
 
-export default Tv;
+/* @ts-expect-error Server Component */
+const Movie: NextPage<IProps> = async ({ params }) => {
+  const { id } = params;
+  const show = await getTv(id);
+
+  return (
+    <S.Container>
+      <S.BackdropContainer isBackdrop={!!show.backdrop_path}>
+        {show.backdrop_path && (
+          <S.Backdrop
+            alt={`Backdrop for ${show.name}`}
+            src={`https://image.tmdb.org/t/p/original${show.backdrop_path}`}
+          />
+        )}
+      </S.BackdropContainer>
+      <S.Main>
+        <div>
+          <S.BackButton href="/">{"< Back"}</S.BackButton>
+          <h1>{show.name}</h1>
+          <S.MovieMeta>
+            <p>{new Date(show.first_air_date).getFullYear()}</p>
+            {show.number_of_seasons && (
+              <>
+                <GoPrimitiveDot size="12px" />
+                <p>{show.number_of_seasons} seasons</p>
+              </>
+            )}
+            {show.genres.length > 0 && (
+              <>
+                <GoPrimitiveDot size="12px" />
+                <p>{show.genres.map((genre) => genre.name).join(", ")}</p>
+              </>
+            )}
+          </S.MovieMeta>
+          {show.overview && <S.Description>{show.overview}</S.Description>}
+          <S.MoreInfo>
+            <S.Rating>
+              <IoMdStar />
+              <p>{show.vote_average.toFixed(1)}</p>
+            </S.Rating>
+            <S.Separator size="12px" />
+            <S.Links>
+              <a
+                href={`https://www.themoviedb.org/tv/${show.id}-${paramCase(
+                  show.name
+                )}`}
+              >
+                <img src={tmdbLogo.src} alt="The Movie DB" />
+              </a>
+            </S.Links>
+          </S.MoreInfo>
+          {/* <PlatformSelect movieId={movie.id} /> */}
+        </div>
+      </S.Main>
+    </S.Container>
+  );
+};
+
+export default Movie;
---
+++ src/pages/api/tv.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import ITv from "@/types/ITv";
+import apiUrlBuilder from "@/utils/apiUrlBuilder";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ITv | null>
+) => {
+  if (!req.query.id || typeof req.query.id !== "string") {
+    return res.status(400);
+  }
+
+  const { id } = req.query;
+
+  const request = await fetch(
+    apiUrlBuilder(`/tv/${id}`, {
+      language: "en-US",
+      append_to_response: "watch/providers",
+    })
+  );
+
+  if (!request.ok) {
+    return res.status(404).json(null);
+  }
+
+  const result = await request.json();
+
+  return res.json(result);
+};
+
+export default handler;
