24d38726fcedecd9bb5e27e481b13f17fcece35e
Theo Sandell <theo01sandell@gmail.com>
2023-02-10 13:47:28 +0100

    Update urlBuilder.ts

--- src/utils/urlBuilder.ts
+++ src/utils/urlBuilder.ts
@@ -1,11 +1,11 @@
+import { APP_URL } from "@/lib/constants";
+
 const urlBuilder = (endpoint: string, query?: { [key: string]: any }) => {
   if (endpoint.startsWith("/")) {
     endpoint.slice(1);
   }
 
-  const queryItems = new URLSearchParams({
-    api_key: process.env.TMDB_API_KEY,
-  });
+  const queryItems = new URLSearchParams({});
 
   if (query) {
     for (const key of Object.keys(query)) {
@@ -13,7 +13,7 @@ const urlBuilder = (endpoint: string, query?: { [key: string]: any }) => {
     }
   }
 
-  return `${process.env.APP_URL}${endpoint}?${queryItems.toString()}`;
+  return `${APP_URL}${endpoint}?${queryItems.toString()}`;
 };
 
 export default urlBuilder;
