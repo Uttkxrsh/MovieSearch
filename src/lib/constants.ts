6d76f7f189e9c9339f994c19a045f6b169c2e1b2
Theo Sandell <theo01sandell@gmail.com>
2023-02-13 16:36:55 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const APP_URL = !!process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/`
   : process.env.NEXT_PUBLIC_APP_URL;
 
 export const APP_TITLE = "Where to watch";
